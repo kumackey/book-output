9348507d85087a8c372ae785b2a5c7a981bf0cb1
kumackey <kumak1t09e0@gmail.com>
2020-02-10 11:43:11 +0900

    users tableの追加

---
+++ db/migrate/20200210023113_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    def change
+      create_table :users do |t|
+        t.string :username, null: false
+        t.string :email,            null: false
+        t.string :crypted_password
+        t.string :salt
+  
+        t.timestamps                null: false
+      end
+  
+      add_index :users, :email, unique: true
+    end
+  end
+end
