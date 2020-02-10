d4153570cc070c5a8b75037187d5300ea5c43ad7
kumackey <kumak1t09e0@gmail.com>
2020-02-10 09:26:36 +0900

    g controllerの設定

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,22 @@ module BookOutput
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.template_engine = :slim
+      g.assets false
+      g.skip_routes true
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
   end
 end
