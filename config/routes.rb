8d2893a75b745661adc1d0e25b9370348e75dd19
kumackey <kumak1t09e0@gmail.com>
2020-02-12 20:44:52 +0900

    ログイン画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'users#new'
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 end
