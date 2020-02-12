f171f2d659122f47b63702a6f4bbd4f9d8d12a38
kumackey <kumak1t09e0@gmail.com>
2020-02-12 19:56:45 +0900

    users#createの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      auto_login @user
+      redirect_to signup_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :username)
+  end
 end
