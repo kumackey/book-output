8d18441af47fe4bf81a8140493820e67e7966d5e
kumackey <kumak1t09e0@gmail.com>
2020-02-28 09:16:00 +0900

    sign_in,sign_upへ統一

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'users#new'
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
+  get 'sign_up', to: 'users#new'
+  post 'sign_up', to: 'users#create'
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
   resources :books, shallow: true do
     collection do
       get :search
