83e564e2b6c6e0d5c8f85375d263064db9069342
kumackey <kumak1t09e0@gmail.com>
2020-02-29 10:04:27 +0900

    S3への画像保存設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
