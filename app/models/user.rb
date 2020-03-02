e72b70c68382c7d0d5a51afd717b9d9f28b10fc1
kumackey <kumak1t09e0@gmail.com>
2020-03-02 17:52:53 +0900

    一旦さしもどし

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@
 
 class User < ApplicationRecord
   authenticates_with_sorcery!
-  has_many :books, dependent: destroy
+  has_many :books
 
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
