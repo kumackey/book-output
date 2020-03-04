5ffaa068aa799e3810d2a2d6d74de6732195c390
kumackey <kumak1t09e0@gmail.com>
2020-03-04 16:57:59 +0900

    検索キーワードが適当なときに、検索結果を返さない

--- app/forms/search_books_form.rb
+++ app/forms/search_books_form.rb
@@ -14,10 +14,13 @@ class SearchBooksForm
     def search(keyword)
       url = url_of_searching_from_keyword(keyword)
       json = get_json_from_url(url)
-      items = json['items']
       books = []
-      items.each do |item|
-        books << GoogleBook.new(item)
+      if json['items']
+        items = json['items']
+        books = []
+        items.each do |item|
+          books << GoogleBook.new(item)
+        end
       end
       books
     end
