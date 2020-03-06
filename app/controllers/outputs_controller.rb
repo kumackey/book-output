21cf487d878f791bf5079baef9e3b1ea7c9b6717
kumackey <kumak1t09e0@gmail.com>
2020-03-06 15:14:11 +0900

    outputs#update

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -19,6 +19,11 @@ class OutputsController < ApplicationController
     @output = current_user.outputs.find(params[:id])
   end
 
+  def update
+    @output = current_user.outputs.find(params[:id])
+    @output.update(update_output_params)
+  end
+
   def destroy
     @output = current_user.outputs.find(params[:id])
     @output.destroy
@@ -28,5 +33,9 @@ class OutputsController < ApplicationController
 
   def create_output_params
     params.require(:output).permit(:content).merge(book_id: params[:book_id])
+  end
+
+  def update_output_params
+    params.require(:output).permit(:content)
   end
 end
