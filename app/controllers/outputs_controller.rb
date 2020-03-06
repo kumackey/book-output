9b366f62bf0bf3ad7f2db8c3a641b1e2e217783b
kumackey <kumak1t09e0@gmail.com>
2020-03-06 15:27:08 +0900

    outputs#createをajaxに

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -4,15 +4,7 @@ class OutputsController < ApplicationController
   end
 
   def create
-    @output = current_user.outputs.build(create_output_params)
-    book = @output.book
-    if @output.save
-      redirect_to book, success: '投稿しました'
-    else
-      flash.now[:danger] = '投稿に失敗しました'
-      book = @output.book
-      redirect_to book, danger: '投稿に失敗しました'
-    end
+    @output = current_user.outputs.create(create_output_params)
   end
 
   def edit
