03d5666010108deda0c91c82f7049096d43c0f65
kumackey <kumak1t09e0@gmail.com>
2020-03-06 09:36:34 +0900

    outputモデルのmigrate

---
+++ spec/models/output_spec.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: outputs
+#
+#  id         :bigint           not null, primary key
+#  content    :text(65535)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  book_id    :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_outputs_on_book_id  (book_id)
+#  index_outputs_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (book_id => books.id)
+#  fk_rails_...  (user_id => users.id)
+#
+
+require 'rails_helper'
+
+RSpec.describe Output, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
