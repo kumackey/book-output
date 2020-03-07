604c533aa0eac668802f41b13e6c99686e687304
kumackey <kumak1t09e0@gmail.com>
2020-03-07 10:14:54 +0900

    いいねを外せること

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,4 +5,9 @@ class LikesController < ApplicationController
     @book = current_user.books.find(params[:book_id])
     current_user.like(@book)
   end
+
+  def destroy
+    @book = current_user.likes.find(params[:id]).book
+    current_user.unlike(@book)
+  end
 end
