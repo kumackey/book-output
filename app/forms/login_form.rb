73bfd49713a24858ab0bab6473a9b4515d38c501
kumackey <kumak1t09e0@gmail.com>
2020-03-09 14:52:27 +0900

    ログインフォームにバリデーション

--- app/forms/login_form.rb
+++ app/forms/login_form.rb
@@ -1,7 +1,11 @@
 class LoginForm
   include ActiveModel::Model
   include ActiveModel::Attributes
+  include ActiveModel::Validations
 
   attribute :email, :string
   attribute :password, :string
+
+  validates :email, presence: true
+  validates :password, presence: true
 end
