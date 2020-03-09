5552f63480109486e8f528ac56dc25e7194e8ff1
kumackey <kumak1t09e0@gmail.com>
2020-03-09 15:56:47 +0900

    ログインのrequestspec

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe "Sessions", type: :request do
     expect(response).to have_http_status(200)
   end
 
+  it "ログインに成功すること" do
+    user = create(:user, password: 'password', password_confirmation: 'password')
+    post '/login', params: { login_form: { 
+      email: user.email,
+      password: 'password',
+    } }
+    expect(response).to redirect_to root_path
+  end
+
   it "ゲストログインに成功すること" do
     post '/guest_login'
     expect(response).to redirect_to root_path
