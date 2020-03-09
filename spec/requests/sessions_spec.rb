6d24c3e057dbb8723b2b6b5ddb9a35f4e3a83ece
kumackey <kumak1t09e0@gmail.com>
2020-03-09 11:37:58 +0900

    guest_loginのリクエストスペック

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,18 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
+  
   it "ログイン画面の表示に成功すること" do
     get '/login'
     expect(response).to have_http_status(200)
   end
 
   it "ゲストログインに成功すること" do
-    create(:user, 
-      email: "guest@guest.jp",
-      password: "guestguest",
-      password_confirmation: "guestguest"
-    )
-    post '/guest'
+    post '/guest_login'
     expect(response).to redirect_to root_path
   end
 end
