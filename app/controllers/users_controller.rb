8100da2a5f58d9dd1919469d587587cb32f0b38a
kumackey <kumak1t09e0@gmail.com>
2020-03-09 13:49:31 +0900

    ユーザ作成時にrootに行くよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login @user
-      redirect_to signup_path, success: 'ユーザーを作成しました'
+      redirect_to root_path, success: 'ユーザーを作成し、ログインしました'
     else
       flash.now[:danger] = 'ユーザーの作成に失敗しました'
       render :new
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,9 +4,16 @@ RSpec.describe "Users", type: :request do
   it "ユーザ作成画面の表示に成功すること" do
     get '/signup'
     expect(response).to have_http_status(200)
+  end
 
-    get '/users/new'
-    expect(response).to have_http_status(200)
+  it "ユーザの作成" do
+    post '/signup', params: { user: { 
+      username: Faker::Name.name,
+      email: Faker::Internet.unique.email,
+      password: 'password',
+      password_confirmation: 'password'
+    } }
+    expect(response).to redirect_to root_path
   end
 end
 
