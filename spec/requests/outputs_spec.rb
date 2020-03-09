c2191c70432ce4cc3d0397e6ebc045a3b3a0318e
kumackey <kumak1t09e0@gmail.com>
2020-03-09 13:14:20 +0900

    ログインロジックをrequestspecに盛り込む

--- spec/requests/outputs_spec.rb
+++ spec/requests/outputs_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Outputs", type: :request do
-  # ログインしてないと不可
-  # it '問題作成画面の表示に成功すること' do
-  #   book = create(:book)
-  #   get "/books/#{book.id}/outputs/new"
-  #   expect(response).to have_http_status(200)
-  # end
+  it '問題作成画面の表示に成功すること' do
+    book = create(:book)
+    user = book.user
+    login_user(user, 'password', login_path)
+    get "/books/#{book.id}/outputs/new"
+    expect(response).to have_http_status(200)
+  end
 
   it '問題表示画面の表示に成功すること' do
     output = create(:output)
