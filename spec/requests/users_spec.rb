617ef82734eb8e1f51d8684c0281436eccf8a1db
kumackey <kumak1t09e0@gmail.com>
2020-03-09 17:08:42 +0900

    usersに対するrequest specを強固に

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe "Users", type: :request do
   it "ユーザ作成画面の表示に成功すること" do
     get '/signup'
     expect(response).to have_http_status(200)
+    expect(response.body).to include("ユーザー登録")
   end
 
-  it "ユーザの作成" do
+  it "ユーザの作成を行えること" do
     post '/signup', params: { user: { 
       username: Faker::Name.name,
       email: Faker::Internet.unique.email,
@@ -14,6 +15,8 @@ RSpec.describe "Users", type: :request do
       password_confirmation: 'password'
     } }
     expect(response).to redirect_to root_path
+    follow_redirect!
+    expect(response.body).to include('ユーザーを作成し')
   end
 end
 
