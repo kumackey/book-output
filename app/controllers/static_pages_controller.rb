25f61aab6063d26b4167a94fd4efd8d1f256aaaa
kumackey <kumak1t09e0@gmail.com>
2020-03-10 22:17:52 +0900

    重複箇所の修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
 
   def home
     # ログイン時のトップページ
-    @outputs = current_user.feed.includes(%i[user book]).order(created_at: :desc).page(params[:page]).per(10).order(created_at: :desc)
+    @outputs = current_user.feed.includes(%i[user book]).page(params[:page]).per(10).order(created_at: :desc)
     @books = Book.includes(:user).order(created_at: :desc).limit(5)
     @outputs_count = @outputs.size
   end
