0a6fe5f8ceb4058f24477765be3176a67347c50a
kumackey <kumak1t09e0@gmail.com>
2020-03-10 22:10:00 +0900

    クイズ一覧ページ

--- spec/requests/outputs_spec.rb
+++ spec/requests/outputs_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Outputs", type: :request do
-  it '問題作成画面の表示に成功すること' do
+  it 'クイズ一覧画面の表示に成功すること' do
+    get "/outputs"
+    expect(response).to have_http_status(200)
+  end
+
+  it 'クイズ作成画面の表示に成功すること' do
     book = create(:book, title: "ファスト＆スロー(下)")
     login
     get "/books/#{book.id}/outputs/new"
@@ -9,7 +14,7 @@ RSpec.describe "Outputs", type: :request do
     expect(response.body).to include("ファスト＆スロー(下)")
   end
 
-  it '問題表示画面の表示に成功すること' do
+  it 'クイズ表示画面の表示に成功すること' do
     output = create(:output, content: "Outputしています。")
     get "/outputs/#{output.id}"
     expect(response).to have_http_status(200)
