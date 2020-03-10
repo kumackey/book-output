ebde8ad340ae846a77e49dedf99523d5a3ac9e40
kumackey <kumak1t09e0@gmail.com>
2020-03-10 19:55:34 +0900

    トップページにfeedを表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,8 @@ class StaticPagesController < ApplicationController
 
   def home
     # ログイン時のトップページ
-    @outputs = Output.all.includes(%i[user book]).page(params[:page]).per(15).order(created_at: :desc)
-    @books = Book.all.includes(:user).page(params[:page]).per(15).order(created_at: :desc)
+    @outputs = current_user.feed.page(params[:page]).per(10).order(created_at: :desc)
+    @books = current_user.like_books.page(params[:page]).per(6).order(created_at: :desc)
   end
 
   def top
