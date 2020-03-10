9e9e93659d21d568031a82d30f7397dba0ac1bee
kumackey <kumak1t09e0@gmail.com>
2020-03-10 21:46:16 +0900

    別のユーザの本がいいね出来なかった不具合

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   before_action :require_login, only: %i[create destroy]
 
   def create
-    @book = current_user.books.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     current_user.like(@book)
   end
 
