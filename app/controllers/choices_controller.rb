0f3fc8830cab543b03348d27ec13960bd6d16085
kumackey <kumak1t09e0@gmail.com>
2020-03-16 07:39:44 +0900

    controllerの書き換え

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,12 +1,12 @@
 class ChoicesController < ApplicationController
   def check
     @choice = Choice.find(params[:id])
-    @output = @choice.output
-    @book = @output.book
+    @question = @choice.question
+    @book = @question.book
     if @choice.is_answer.present?
       redirect_to @book, success: '正解です！'
     else
-      redirect_to @output, danger: '残念！不正解です・・・'
+      redirect_to @question, danger: '残念！不正解です・・・'
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,18 +22,18 @@ class UsersController < ApplicationController
     @user = current_user
     respond_to do |format|
       format.html do
-        @outputs = @user.outputs.includes(%i[user book]).page(params[:page]).per(10).order(created_at: :desc)
+        @questions = @user.questions.includes(%i[user book]).page(params[:page]).per(10).order(created_at: :desc)
         @books = @user.like_books.includes(%i[user book]).order(created_at: :desc)
       end
       format.csv do
-        @outputs = @user.outputs.includes(%i[user book]).order(created_at: :desc)
+        @questions = @user.questions.includes(%i[user book]).order(created_at: :desc)
       end
     end
   end
 
   def show
     @user = User.find(params[:id])
-    @outputs = @user.outputs.includes(%i[user book]).page(params[:page]).per(10).order(created_at: :desc)
+    @questions = @user.questions.includes(%i[user book]).page(params[:page]).per(10).order(created_at: :desc)
     @books = @user.like_books.includes(:user).order(created_at: :desc)
   end
 
