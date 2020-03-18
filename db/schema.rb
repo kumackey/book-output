9d9da1fb971847c195c386ed8dd0e7329811a5c9
kumackey <kumak1t09e0@gmail.com>
2020-03-18 12:31:24 +0900

    avatarカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_063140) do
+ActiveRecord::Schema.define(version: 2020_03_18_032923) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "googlebooksapi_id", null: false
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_03_16_063140) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
