8043379abc9feb7006ed8d9c9b6e2bcf1cf33394
kumackey <kumak1t09e0@gmail.com>
2020-03-18 12:42:24 +0900

    モデルにavatarの設定追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@
 
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  mount_uploader :avatar, AvatarUploader
+
   has_many :books, dependent: :destroy
   has_many :questions, dependent: :destroy
   has_many :likes, dependent: :destroy
