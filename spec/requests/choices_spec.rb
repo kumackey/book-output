2ef04f7d598c8450f26f2bc304921680b1addafa
kumackey <kumak1t09e0@gmail.com>
2020-03-23 09:12:51 +0900

    can show when random question and not logged in

--- spec/requests/choices_spec.rb
+++ spec/requests/choices_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Choces", type: :request do
   it '選択肢の回答結果画面の表示に成功すること' do
-    login
     choice = create(:choice)
     get "/choices/#{choice.id}"
     expect(response).to have_http_status(200)
