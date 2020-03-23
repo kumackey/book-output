675071246eb3dcd1ae9e77229dd20eb92d1cf21d
kumackey <kumak1t09e0@gmail.com>
2020-03-23 11:43:14 +0900

    test of likes#destroy

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -6,5 +6,13 @@ RSpec.describe "Likes", type: :request do
     book = create(:book)
     expect { post '/likes', params: { book_id: book.id }, xhr: true }.to change{ Like.count }.by(1)
   end
+
+  it '本のいいねを削除できること' do
+    user = login
+    book = create(:book)
+    user.like(book)
+    like = Like.find_by(user_id: user.id, book_id: book.id)
+    expect { delete "/likes/#{like.id}", xhr: true }.to change{ Like.count }.by(-1)
+  end
 end
 
