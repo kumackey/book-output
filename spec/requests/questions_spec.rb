7dae989accb2d042deec186def16b5465f0fe03b
kumackey <kumak1t09e0@gmail.com>
2020-03-23 09:25:50 +0900

    test of questions#create

--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Questions", type: :request do
+
+  it 'クイズの作成に成功すること' do
+    login
+    user = create(:user)
+    book = create(:book)
+    post "/books/#{book.id}/questions", params: { register_output_form: { 
+      question_content: "学校補助金の増額案に対する賛成票は、ある投票所では優位に多かった。どこか？",
+      commentary: "プライミング効果によって、学校に対して有利な投票をしたくなるため",
+      user_id: user.id,
+      book_id: book.id,
+      correct_choice: "学校",
+      incorrect_choice_1: "市役所",
+      incorrect_choice_2: "警察署",
+      incorrect_choice_3: "美術館",
+    } }
+    expect(response).to redirect_to book
+    follow_redirect!
+    expect(response.body).to include('学校補助金の増額案に')
+  end
+
   it 'クイズ一覧画面の表示に成功すること' do
     get "/questions"
     expect(response).to have_http_status(200)
