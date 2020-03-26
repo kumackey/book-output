15c6c5bfde9f57410eb1e5c3b02b2db324d72eb5
kumackey <kumak1t09e0@gmail.com>
2020-03-26 15:48:52 +0900

    change following routes change

--- app/controllers/mypage/like_books_controller.rb
+++ app/controllers/mypage/like_books_controller.rb
@@ -1,5 +1,5 @@
 class Mypage::LikeBooksController < Mypage::BaseController
-  def show
+  def index
     @user = User.find(current_user.id)
     @books = @user.like_books.includes(:user).order(created_at: :desc)
   end
--- spec/requests/mypage/questions_spec.rb
+++ spec/requests/mypage/questions_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "MakeQuestions", type: :request do
+RSpec.describe "Questions", type: :request do
   it "作成したクイズ一覧の画面の取得に成功すること" do
     login
-    get "/mypage/make_questions"
+    get "/mypage/questions"
     expect(response).to have_http_status(200)
   end
 end
