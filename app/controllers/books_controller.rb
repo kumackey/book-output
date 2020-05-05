d5cfac542a7a9830d70b091b73700bfecbea81da
kumackey <kumak1t09e0@gmail.com>
2020-05-05 22:07:43 +0900

    R.I.P. books#new, books#create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,4 @@
 class BooksController < ApplicationController
-  before_action :require_login, only: %i[create]
-
-  def create
-    google_book = GoogleBook.new_from_id(create_book_params[:googlebooksapi_id])
-    @book = google_book.build_book_by_user(current_user)
-    if @book.save
-      current_user.like(@book)
-      redirect_to books_path, success: '本を登録しました'
-    else
-      flash.now[:danger] = '本の登録に失敗しました'
-      render :search_books_path
-    end
-  end
-
-  def new
-    @google_book = GoogleBook.new_from_id(create_book_params[:googlebooksapi_id])
-    @book = Book.find_by(googlebooksapi_id: @google_book.googlebooksapi_id)
-  end
-
   def show
     @book = GoogleBook.new_from_id(params[:id])
     @questions = @book.questions.includes(:user).order(created_at: :desc).page(params[:page]).per(8)
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -39,14 +39,6 @@ RSpec.describe "Books", type: :request do
     end
   end
 
-  it '本の登録に成功すること' do
-    login
-    post '/books', params: { googlebooksapi_id: "xPbRxgEACAAJ" }
-    expect(response).to redirect_to books_path
-    follow_redirect!
-    expect(response.body).to include('本を登録しました')
-  end
-
   it '本詳細画面の表示に成功すること' do
     book = create(:book, title: "ファスト＆スロー(下)")
     create(:other_book, title: "影響力の武器")
