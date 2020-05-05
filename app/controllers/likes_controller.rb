ddad4f6c1a8043c690d01033e4c34a01aefd28c6
kumackey <kumak1t09e0@gmail.com>
2020-05-05 21:41:30 +0900

    Change View of likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   before_action :require_login, only: %i[create destroy]
 
   def create
-    @book = Book.find(params[:book_id])
-    current_user.like(@book)
+    @book = GoogleBook.new_from_id(params[:book_id])
+    current_user.like_google_book(@book)
   end
 
   def destroy
-    @book = current_user.likes.find(params[:id]).book
-    current_user.unlike(@book)
+    @book = current_user.likes.find(params[:id]).google_book
+    current_user.unlike_google_book(@book)
   end
 end
--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe "Likes", type: :request do
   it '本にいいねができること' do
     login
-    book = create(:book)
-    expect { post '/likes', params: { book_id: book.id }, xhr: true }.to change{ Like.count }.by(1)
+    book = GoogleBook.new_from_id("4j13KIu3Z60C")
+    expect { post '/likes', params: { book_id: book.googlebooksapi_id }, xhr: true }.to change{ Like.count }.by(1)
   end
 
   it '本のいいねを削除できること' do
     user = login
-    book = create(:book)
-    user.like(book)
-    like = Like.find_by(user_id: user.id, book_id: book.id)
+    book = GoogleBook.new_from_id("4j13KIu3Z60C")
+    user.like_google_book(book)
+    like = Like.find_by(user_id: user.id, book_id: book.googlebooksapi_id)
     expect { delete "/likes/#{like.id}", xhr: true }.to change{ Like.count }.by(-1)
   end
 end
