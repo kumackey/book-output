ab3b716b1895e5b253506050c387b0803f9f9f28
kumackey <kumak1t09e0@gmail.com>
2020-05-06 10:26:45 +0900

    Fix choices#show

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,7 +1,7 @@
 class ChoicesController < ApplicationController
   def show
     @choice = Choice.find(params[:id])
-    @book = @choice.question.book
+    @book = @choice.question.google_book
     @choices = @choice.question.choices.includes(:question).order(:created_at)
     render layout: 'book_detail'
   end
