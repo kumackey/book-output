eb8112d698766683d5056631972e17a57f29b40c
kumackey <kumak1t09e0@gmail.com>
2020-05-09 08:26:20 +0900

    Add answer_content test

--- spec/forms/only_answer_quiz_form_spec.rb
+++ spec/forms/only_answer_quiz_form_spec.rb
@@ -71,4 +71,28 @@ RSpec.describe OnlyAnswerQuizForm, type: :model do
       end
     end
   end
+
+  describe '解答が' do
+    context '無いときに' do
+      let(:only_answer_quiz_form) { build(:only_answer_quiz_form, answer_content: nil) }
+      it '無効なこと' do
+        only_answer_quiz_form.valid?
+        expect(only_answer_quiz_form.errors.messages[:answer_content]).to include('を入力してください')
+      end
+    end
+    ANSWER_MAX_WORD_COUNT = 40
+    context "#{QUESTION_MAX_WORD_COUNT}文字のときに" do
+      let(:only_answer_quiz_form) { build(:only_answer_quiz_form, answer_content: 'a' * ANSWER_MAX_WORD_COUNT) }
+      it '有効なこと' do
+        expect(only_answer_quiz_form).to be_valid
+      end
+    end
+    context "#{ANSWER_MAX_WORD_COUNT + 1}文字のときに" do
+      let(:only_answer_quiz_form) { build(:only_answer_quiz_form, answer_content: 'a' * (ANSWER_MAX_WORD_COUNT + 1)) }
+      it '無効なこと' do
+        only_answer_quiz_form.valid?
+        expect(only_answer_quiz_form.errors.messages[:answer_content]).to include("は#{ANSWER_MAX_WORD_COUNT}文字以内で入力してください")
+      end
+    end
+  end
 end
