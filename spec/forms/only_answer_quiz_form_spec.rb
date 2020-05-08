a150c052eed7813a0c5919ec930389817efd6217
kumackey <kumak1t09e0@gmail.com>
2020-05-09 08:23:42 +0900

    improve how to test OnlyAnswerQuizForm

--- spec/forms/only_answer_quiz_form_spec.rb
+++ spec/forms/only_answer_quiz_form_spec.rb
@@ -8,18 +8,21 @@ RSpec.describe OnlyAnswerQuizForm, type: :model do
   end
 
   it '質問文が無い時に無効なこと' do
-    only_answer_quiz_form.question_content = nil
-    expect(only_answer_quiz_form).not_to be_valid
+    only_answer_quiz_form = build(:only_answer_quiz_form, question_content: nil)
+    only_answer_quiz_form.valid?
+    expect(only_answer_quiz_form.errors.messages[:question_content]).to include('を入力してください')
   end
 
   it '本が無い時に無効なこと' do
-    only_answer_quiz_form.book_id = nil
-    expect(only_answer_quiz_form).not_to be_valid
+    only_answer_quiz_form = build(:only_answer_quiz_form, book_id: nil)
+    only_answer_quiz_form.valid?
+    expect(only_answer_quiz_form.errors.messages[:book_id]).to include('を入力してください')
   end
 
   it '投稿者が無い時に無効なこと' do
-    only_answer_quiz_form.user_id = nil
-    expect(only_answer_quiz_form).not_to be_valid
+    only_answer_quiz_form = build(:only_answer_quiz_form, user_id: nil)
+    only_answer_quiz_form.valid?
+    expect(only_answer_quiz_form.errors.messages[:user_id]).to include('を入力してください')
   end
 
   describe '解説文が' do
@@ -29,22 +32,43 @@ RSpec.describe OnlyAnswerQuizForm, type: :model do
         expect(only_answer_quiz_form).to be_valid
       end
     end
-    context '255文字のときに' do
-      let(:only_answer_quiz_form) { build(:only_answer_quiz_form, commentary: 'a' * 255) }
+    COMMENTARY_MAX_WORD_COUNT = 255
+    context "#{COMMENTARY_MAX_WORD_COUNT}文字のときに" do
+      let(:only_answer_quiz_form) { build(:only_answer_quiz_form, commentary: 'a' * COMMENTARY_MAX_WORD_COUNT) }
       it '有効なこと' do
         expect(only_answer_quiz_form).to be_valid
       end
     end
-    context '256文字のときに' do
-      let(:only_answer_quiz_form) { build(:only_answer_quiz_form, commentary: 'a' * 256) }
+    context "#{COMMENTARY_MAX_WORD_COUNT + 1}文字のときに" do
+      let(:only_answer_quiz_form) { build(:only_answer_quiz_form, commentary: 'a' * (COMMENTARY_MAX_WORD_COUNT + 1)) }
       it '無効なこと' do
-        expect(only_answer_quiz_form).not_to be_valid
+        only_answer_quiz_form.valid?
+        expect(only_answer_quiz_form.errors.messages[:commentary]).to include("は#{COMMENTARY_MAX_WORD_COUNT}文字以内で入力してください")
       end
     end
   end
 
-  it '問題文が141文字のときに無効なこと' do
-    only_answer_quiz_form.question_content = 'a' * 141
-    expect(only_answer_quiz_form).not_to be_valid
+  describe '問題文が' do
+    context '無いときに' do
+      let(:only_answer_quiz_form) { build(:only_answer_quiz_form, question_content: nil) }
+      it '無効なこと' do
+        only_answer_quiz_form.valid?
+        expect(only_answer_quiz_form.errors.messages[:question_content]).to include('を入力してください')
+      end
+    end
+    QUESTION_MAX_WORD_COUNT = 140
+    context "#{QUESTION_MAX_WORD_COUNT}文字のときに" do
+      let(:only_answer_quiz_form) { build(:only_answer_quiz_form, question_content: 'a' * QUESTION_MAX_WORD_COUNT) }
+      it '有効なこと' do
+        expect(only_answer_quiz_form).to be_valid
+      end
+    end
+    context "#{QUESTION_MAX_WORD_COUNT + 1}文字のときに" do
+      let(:only_answer_quiz_form) { build(:only_answer_quiz_form, question_content: 'a' * (QUESTION_MAX_WORD_COUNT + 1)) }
+      it '無効なこと' do
+        only_answer_quiz_form.valid?
+        expect(only_answer_quiz_form.errors.messages[:question_content]).to include("は#{QUESTION_MAX_WORD_COUNT}文字以内で入力してください")
+      end
+    end
   end
 end
