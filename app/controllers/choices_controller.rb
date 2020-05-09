b1c2999b7bccbb8093c0a0bb5839cde863916160
kumackey <kumak1t09e0@gmail.com>
2020-05-09 18:13:05 +0900

    rubocop -a

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,10 +1,10 @@
 class ChoicesController < ApplicationController
   def show
     @choice = Choice.find(params[:id])
-    if @choice.question.answer_type == 'choices'
-      @book = @choice.question.book
-      @choices = @choice.question.choices.includes(:question).order(:created_at)
-      render layout: 'book_detail'
-    end
+    return unless @choice.question.answer_type == 'choices'
+
+    @book = @choice.question.book
+    @choices = @choice.question.choices.includes(:question).order(:created_at)
+    render layout: 'book_detail'
   end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -19,6 +19,6 @@
 
 class Answer < ApplicationRecord
   belongs_to :question
-  
+
   validates :content, presence: true, length: { maximum: 40 }
 end
