c8bb115bfd099e7965044208ff829020d2f8c320
kumackey <kumak1t09e0@gmail.com>
2020-05-09 15:00:31 +0900

    implement OnlyAnswerQuizs#create

--- app/controllers/only_answer_quizs_controller.rb
+++ app/controllers/only_answer_quizs_controller.rb
@@ -6,4 +6,23 @@ class OnlyAnswerQuizsController < ApplicationController
     @only_answer_quiz_form = OnlyAnswerQuizForm.new
     render layout: 'book_detail'
   end
+
+  def create
+    @book = Book.find(params[:book_id])
+    @only_answer_quiz_form = OnlyAnswerQuizForm.new(create_question_params)
+    if @only_answer_quiz_form.save
+      redirect_to @book, success: '問題を作成しました'
+    else
+      flash.now[:danger] = '問題の作成に失敗しました'
+      render layout: 'book_detail', action: :new
+    end
+  end
+
+  private
+
+  def create_question_params
+    params.require(:only_answer_quiz_form)
+          .permit(:question_content, :answer_content, :commentary)
+          .merge(user_id: current_user.id, book_id: params[:book_id])
+  end
 end
--- spec/requests/only_answer_quizs_spec.rb
+++ spec/requests/only_answer_quizs_spec.rb
@@ -1,6 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Only_answer_quizs', type: :request do
+  it '適切なパラメータが渡されているときにクイズを作成できること' do
+    login
+    user = create(:user)
+    book = create(:book)
+    QUESTION_CONTENT = '学校補助金の増額案に対する賛成票は、ある投票所では優位に多かった。どこか？'.freeze
+    expect { post "/books/#{book.id}/only_answer_quizs", params: { only_answer_quiz_form: {
+      question_content: QUESTION_CONTENT,
+      commentary: 'プライミング効果によって、学校に対して有利な投票をしたくなるため',
+      user_id: user.id,
+      book_id: book.id,
+      answer_content: '学校'
+    } }
+    } .to change { Question.count }.by(1)
+    expect(response).to redirect_to book
+    follow_redirect!
+    expect(response.body).to include(QUESTION_CONTENT)
+  end
+
+  it '不適切なパラメータしか渡されていないときにクイズの作成に失敗すること' do
+    login
+    user = create(:user)
+    book = create(:book)
+    expect { post "/books/#{book.id}/only_answer_quizs", params: { only_answer_quiz_form: {
+      question_content: nil, #  question_contentがない
+      commentary: 'プライミング効果によって、学校に対して有利な投票をしたくなるため',
+      user_id: user.id,
+      book_id: book.id,
+      answer_content: '学校'
+    } }
+    } .to change { Question.count }.by(0)
+    expect(response).to have_http_status(200)
+    expect(response.body).to include('問題の作成に失敗しました')
+  end
+
   it 'クイズ作成画面が取得できること' do
     BOOK_TITLE = 'ファスト＆スロー(下)'.freeze
     book = create(:book, title: BOOK_TITLE)
