524e4871195d69a732b7322bd9b84e8efdaf3947
kumackey <kumak1t09e0@gmail.com>
2020-05-10 22:00:33 +0900

    Add admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
 
+  rescue_from SecurityError do |exception|
+    redirect_to root_path, danger: '管理者画面へのアクセス権限がありません'
+  end
+
   def not_authenticated
     redirect_to login_path, warning: 'ログインしてください'
   end
 
   def authenticated
     redirect_to home_path if logged_in?
+  end
+
+  protected
+
+  def authenticate_admin_user!
+    raise SecurityError unless current_user.try(:admin?)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :like_books, through: :likes, source: :book
 
+  enum  role:  {  user:  0,  admin:  1  }
+
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
